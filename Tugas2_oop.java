51455bc69d7f021b431bcbee8a40a56c9c52388d
EkaSepiantini32 <103258422+EkaSepiantini32@users.noreply.github.com>
2022-04-15 12:23:54 +0800

    Kumpul Tugas 2

---
+++ Tugas2_oop.java
@@ -0,0 +1,20 @@
+package tugas2_oop;
+
+/**
+ *
+ * @author Ni Putu Eka Sepiantini 20103295
+ */
+public class Tugas2_oop {
+
+    public static void main(String[] args) {
+        HitungVolumeBalok Balok = new HitungVolumeBalok(4,5,7);
+        
+        //Balok.setPANJANG(4);
+        //Balok.setLEBAR(5);
+        //Balok.setTINGGI(7);
+        
+        Balok.PrintHasil();
+        
+    }
+    
+}
